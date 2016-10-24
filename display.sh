2cbf4c7c717e97b813fd0e73c23f8335c752d4ea
rahul-kumi <kayrahul@gmail.com>
2016-10-24 20:43:45 +0530

    funtionality handling

--- display.sh
+++ display.sh
@@ -1,15 +1,13 @@
-#! /bin/bash/
+#!/bin/bash
 source config.cfg
 
-echo "Your feed"
+echo -e "Your Photos"
 
-time=100
 curl -s -X GET \
- "https://graph.facebook.com/v2.1/me/photos?fields=link,name,album,likes&limit=5&limit=1000&access_token=$access_token" > djson.data
-
+ "https://graph.facebook.com/v2.2/me/photos?fields=link%2Cname%2Calbum%2Csource%2Clikes&limit=1000&access_token=$access_token" >djson.data
 length=$(jq -r ".data | length" djson.data)
 
-#jq -r ".data[]" json.data
+#jq -r ".data[]" djson.data
 
 length=$(jq -r ".data | length" djson.data)
 
@@ -19,7 +17,8 @@ do
 name="$(jq -r ".data[$i].name" djson.data)"
 album="$(jq -r ".data[$i].album" djson.data)"
 link="$(jq -r ".data[$i].link" djson.data)"
-likes="$(jq -r ".data[$i].likes" json.data)"
+likes="$(jq -r ".data[$i].likes" djson.data)"
+source="$(jq -r ".data[$i].source" djson.data)"
 
 if [ "$name" != "null" ]
 then
@@ -38,7 +37,7 @@ fi
 if [ "$(jq -r ".data[$i].likes" djson.data)" != "null" ]
 then
   echo "Liked by :"
-  for (( k = 0; k < 5 ; k++ ));
+  for (( k = 0; k < 5 ; k++ ))
   do
     if [[ $(jq -r ".data[$i].likes.data[$k].name" djson.data) != "null" ]]; then
       echo $(jq -r ".data[$i].likes.data[$k].name" djson.data)
@@ -46,22 +45,15 @@ then
   done
 fi
 
-#################part to correct
-#the image file will be displayed till its closed or killed by the user
-#fbi is not yet implemented to fecilitate easy testing of the script
-#--TODO:1.handle the filetype
-#-------2.improvise dlearing of the datadump created by the image downloads
+#TODO-1)work needed on automatic display of images
 if [ "$link" != "null" ]
 then
-wget -q $(jq -r ".data[$i].link" djson.data) -o fbimages/"$i"
-  sudo timeout "$time"s display fbimages/"$i"
+  wget -q -O fbimages/"$i.jpg" "$(jq -r ".data[$i].source" djson.data)"
+  sudo fbi fbimages/"$i.jpg"
+  #use`sudo xdg-open fbimages/"$i.jpg"` on virtual consoles
 fi
-#################part to correct
 
 #clear
 printf "\n\n"
 done
-#primitive approach to clear the unwanted files creatd in the folder while downloading files
-rm -r -f photo*
-rm -r -f index*
-#rm json.data
+#rm djson.data
