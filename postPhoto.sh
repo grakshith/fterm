a7e4ca4a2019d5899cbefcc3de085b300ad9f7cf
Revanth B S <bsrevanth2011@gmail.com>
2016-11-03 13:49:43 +0530

    Update postPhoto.sh

--- postPhoto.sh
+++ postPhoto.sh
@@ -1,16 +1,16 @@
 source config.cfg
-echo -e "choose how you would like to upload the file\n 1.url\n 2.file\n"
+echo -e "Choose how you would like to upload the file\n 1.url\n 2.file\n"
 read choice
 if [ $choice -eq 1 ]
 then
-  echo "enter the image url"
+  echo "Enter the image url"
 else
-  echo "enter the location of the file"
+  echo "Enter the location of the file"
 fi
 read $image_location
-echo "enter caption"
+echo "Enter caption"
 read $caption
-echo "your image has been posted successfully..."
+echo "Your image has been posted successfully..."
 if [ $choice -eq 1 ]
 then
   curl -F "access_token=$access_token" \
